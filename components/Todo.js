5d3509b5704b2f5ab19ab48762e4110446f9fef8
MuhamadouCeesay <muhamadou74@gmail.com>
2025-07-22 09:24:17 -0400

    Fix: removed duplicate CSS link and invalid img closing tag

--- components/Todo.js
+++ components/Todo.js
@@ -1,30 +1,3 @@
-// import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-
-// export default class Todo {
-//   constructor(text) {
-//     this.text = text;
-//     this.id = uuidv4();
-//   }
-
-//   generateTodo() {
-//     const todoItem = document.createElement("li");
-//     todoItem.classList.add("todo__item");
-
-//     const label = document.createElement("label");
-//     label.setAttribute("for", this.id);
-//     label.textContent = this.text;
-
-//     const checkbox = document.createElement("input");
-//     checkbox.type = "checkbox";
-//     checkbox.id = this.id;
-
-//     todoItem.appendChild(checkbox);
-//     todoItem.appendChild(label);
-
-//     return todoItem;
-//   }
-// }
-
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 export default class Todo {
